9bf3879138ecc1150302390ac0f119930ae04fe8
VectorSophie <jay7math@gmail.com>
2025-10-14 16:47:28 +0900

    feat(App): Testing console api

--- src/App.js
+++ src/App.js
@@ -1,5 +1,10 @@
+import { Console } from '@woowacourse/mission-utils';
+
 class App {
-  async run() {}
+  async run() {
+    const input = "덧셈할 문자열을 입력해 주세요."
+    const inputString = await Console.readLineAsync(input); //await is crucial for actual user interface
+  }
 }
 
 export default App;
